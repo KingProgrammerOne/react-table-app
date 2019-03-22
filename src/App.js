1063bcbd757795e2bdfb90b92b084b997372763a
webstar <webstar1987923@hotmail.com>
2019-03-22 20:55:16 +0300

    2019/3/22

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
-import 'node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
-var products = [{
-  id: 1,
-  name: "Product1",
-  price: 120
-}, {
-  id: 2,
-  name: "Product2",
-  price: 80
-}];
+import '../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <BootstrapTable data={products} version='4'>
-          <TableHeaderColumn isKey dataField='id'>Product ID</TableHeaderColumn>
-          <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
-          <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
-        </BootstrapTable>
-      </div>
-    );
+const jobs = [];
+
+function addProducts(quantity) {
+  const startId = jobs.length;
+  for (let i = 0; i < quantity; i++) {
+    const id = startId + i;
+    jobs.push({
+      id: id,
+      name: 'Application_Name_ ' + id,
+      price: i % 2 === 0 ? true : false
+    });
   }
 }
 
+addProducts(5);
+
+
+const selectRowProp = {
+  mode: 'checkbox'
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <BootstrapTable data={ jobs } selectRow={ selectRowProp }>
+          <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
+          <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+          <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
+}
 export default App;
