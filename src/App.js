95568ddbf70e4a113de589113894816c8943f90b
webstar <webstar1987923@hotmail.com>
2019-03-22 18:08:38 +0300

    add react-bootstrap-table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import 'node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+var products = [{
+  id: 1,
+  name: "Product1",
+  price: 120
+}, {
+  id: 2,
+  name: "Product2",
+  price: 80
+}];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <BootstrapTable data={products} version='4'>
+          <TableHeaderColumn isKey dataField='id'>Product ID</TableHeaderColumn>
+          <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+          <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+        </BootstrapTable>
       </div>
     );
   }
